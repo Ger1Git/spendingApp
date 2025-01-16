75aa1e31187de07a6acd263a5140712565c6b790
George Geru <george.g@machinas.com>
2025-01-16 11:18:13 +0200

    adjustments on packages and port

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const frontendUrl = process.env.FRONTEND_URL;
 const corsOptions = {
